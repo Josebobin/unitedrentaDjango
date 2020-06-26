b7e0f5de6382a7fe0c9d8335a01335d1721748e8
Josebobin <jose06aj@gmail.com>
2020-06-26 10:56:33 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -150,7 +150,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = 'jose06aj@gmail.com'
 DEFAULT_FROM_EMAIL = 'jose06aj@gmail.com'
 SERVER_EMAIL = 'jose06aj@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hgi#2020'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
